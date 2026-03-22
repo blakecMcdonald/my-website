3b3683263486044be56293160e2508054d47e27b
blakecMcdonald <blake.mcdonald@sjsu.edu>
2026-03-22 19:37:59 -0300

    Create navBar.php

---
+++ navBar.php
@@ -0,0 +1,10 @@
+    <nav class="primary-menu">
+   
+      <ul class="nav navbar-nav">
+         <li class="active"><a href="#">Home</a></li>
+         <li><a href="#">Geography</a></li>
+         <li><a href="#">People</a></li>
+         <li><a href="#">Places to Go</a></li>
+      <ul>
+
+    </nav>
